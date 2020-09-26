dd50de6201093f06df53dd38df67ec68f9c83f8c
Rahul Desai <rahuldesai1999@gmail.com>
2020-09-26 20:16:05 +0530

    improves some things

--- ReadFromGoogle.py
+++ ReadFromGoogle.py
@@ -13,20 +13,3 @@ def readFromGoogle(SPREADSHEET_KEY, WORKSHEET_NAME):
 	df = g2d.download(SPREADSHEET_KEY, WORKSHEET_NAME,credentials=credentials, col_names = True)
 	print("Reading done")
 	return df[['Ticker', 'Company', 'Financial Links', 'Search Key for Company', 'Press Release Links', 'Search Key for Press Release']]
-
-
-# SPREADSHEET_KEY = '1PfDAquK5ioQuWAvK4ujhenN5aqlBBKTRJTOqRmneH_Q'
-# WORKSHEET_NAME = 'Testing'
-
-# df = readFromGoogle(SPREADSHEET_KEY, WORKSHEET_NAME)
-# searckKeysFinancials = []
-# searckKeysPress = []
-
-# for company in df['Company'].to_list():
-# 	searckKeysFinancials.append(company + " com quarter results")
-# 	searckKeysPress.append(company + " com press release invertor relations")
-
-# df['Search Key for Company'] = searckKeysFinancials
-# df['Search Key for Press Release'] = searckKeysPress
-
-# write(df, SPREADSHEET_KEY, WORKSHEET_NAME)
